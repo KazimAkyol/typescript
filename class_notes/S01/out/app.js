3a8f30c370b113215f01dc2ced22f4e1704a17a2
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-29 19:17:55 +0100

    fifth commit

--- class_notes/S01/out/app.js
+++ class_notes/S01/out/app.js
@@ -1,3 +1,12 @@
 let myTuple1;
 myTuple1 = [1, true, "mark"];
 console.log(myTuple1);
+let sinif;
+sinif = [
+    [1, "Gökhan"],
+    [2, "Kazim"],
+    [3, "Muhterem"],
+    [4, "Mark"],
+];
+sinif.push([5, "Fatma"]);
+console.log(sinif);
--- class_notes/S01/src/app.ts
+++ class_notes/S01/src/app.ts
@@ -47,6 +47,19 @@ myTuple1 = [1, true, "mark"];
 
 console.log(myTuple1);
 
+let sinif: [number, string][];
+
+sinif = [
+  [1, "Gökhan"],
+  [2, "Kazim"],
+  [3, "Muhterem"],
+  [4, "Mark"],
+];
+
+sinif.push([5, "Fatma"]);
+
+console.log(sinif);
+
 //!!! Intersection
 
 // type Book = {
