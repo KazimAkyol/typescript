8f90649a946f9ba966d5eae2c2b4f715c4557e24
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-30 15:21:22 +0100

    16.commit

--- class_notes/S02/out/app.js
+++ class_notes/S02/out/app.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+//!!! Classes
+class Student {
+    constructor(code, name) {
+        this.stdCode = code;
+        this.stdName = name;
+    }
+}
+let std1 = new Student(1, "Gokhan");
+let std2 = new Student(2, "Kazim");
+let std3 = new Student(3, "Gokhan");
+console.log(std1);
+console.log(std2);
+console.log(std3);
--- class_notes/S02/src/app.ts
+++ class_notes/S02/src/app.ts
@@ -1,1 +1,18 @@
-console.log("Hello");
+//!!! Classes
+class Student {
+    stdCode: number
+    stdName: string
+
+    constructor (code: number, name:string){
+        this.stdCode = code
+        this.stdName = name
+    }
+}
+
+let std1 = new Student (1, "Gokhan")
+let std2 = new Student (2, "Kazim")
+let std3 = new Student (3, "Gokhan")
+
+console.log(std1)
+console.log(std2)
+console.log(std3)
