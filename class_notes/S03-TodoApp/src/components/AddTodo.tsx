0d8ef7b8eb2d2f6bd140247cce5805b3d5e8cb4a
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-31 20:59:17 +0100

    22.commit

--- class_notes/S03-TodoApp/src/components/AddTodo.tsx
+++ class_notes/S03-TodoApp/src/components/AddTodo.tsx
@@ -1,16 +1,50 @@
+import { Save } from "@mui/icons-material";
 import { Box, Button, TextField } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 export default function AddTodo() {
+
+    // const [task, setTask] = useState<string>("")
+    const [task, setTask] = useState("")
+
+
+    const handleSubmit = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        console.log(e.target.value)
+    }
+
   return (
     <Box
       component="form"
+      onSubmit={handleSubmit}
       sx={{
         display: { xs: "block", sm: "flex" },
+        justifyContent: { xs: "flex-start", sm: "center" },
+        m: { xs: 1, sm: "auto" },
+        height: { xs: "120px", sm: "80px" },
       }}
     >
-      <TextField />
-      <Button>Save Todo</Button>
+      <TextField
+        variant="outlined"
+        color="success"
+        sx={{
+          minWidth: { xs: "100%", sm: "50%" },
+          height: "50px",
+          m: 1,
+        }}
+        // onChange={(e)=>console.log(e.target.value)}
+      />
+      <Button
+        variant="contained"
+        endIcon={<Save />}
+        color="success"
+        sx={{
+          minWidth: { xs: "100%", sm: "15%" },
+          height: "55px",
+          m: 1,
+        }}
+      >
+        Save Todo
+      </Button>
     </Box>
   );
 }
--- class_notes/S03-TodoApp/src/pages/Home.tsx
+++ class_notes/S03-TodoApp/src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import { Container } from "@mui/material";
 import React from "react";
 import Header from "../components/Header";
+import AddTodo from "../components/AddTodo";
 
 export default function Home() {
   return (
     <Container>
       <Header />
-      <h1>addTodo</h1>
+      <AddTodo />
       <h1>todoList</h1>
     </Container>
   );
