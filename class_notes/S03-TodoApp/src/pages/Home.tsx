ccd1ea1d5a1e2682fad374492942bab5898fab93
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-31 22:01:16 +0100

    24.commit

--- class_notes/S03-TodoApp/src/pages/Home.tsx
+++ class_notes/S03-TodoApp/src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import { Container } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import AddTodo from "../components/AddTodo";
+import axios from "axios";
+import { error } from "console";
+import TodoList from "../components/TodoList";
 
 const url = "https://64ecd95ff9b2b70f2bfb0943.mockapi.io/todos";
 
@@ -14,11 +17,24 @@ interface ITodo {
 export default function Home() {
   const [todos, setTodos] = useState<ITodo[]>([]);
 
+  const getTodos = async () => {
+    try {
+      const { data } = await axios(url);
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <Container>
       <Header />
       <AddTodo />
-      <h1>todoList</h1>
+      <TodoList todos={todos} />
     </Container>
   );
 }
--- class_notes/S03-TodoApp/src/types.d.ts
+++ class_notes/S03-TodoApp/src/types.d.ts
@@ -1,8 +1,7 @@
-interface ITodoType {
-    task:string;
-    isDone:boolean;
-    id:string | number; //* id değeri string ya da number olabilir
-    owner?: string //! bu alan zorunlu değil, optional. Eğer varsa da type'ı string
+interface ITodo {
+  task: string;
+  isDone: boolean;
+  id: string;
 }
 
 type AddFn = (task: string) => Promise<void>;
